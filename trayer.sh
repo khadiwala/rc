f866d9db876193b59d0b8eba1a0bcaa7886a42e7
rkhadiwala <rkhadiwala@cleversafe.com>
2014-02-14 23:26:26 -0600

    trayer

---
+++ trayer.sh
@@ -0,0 +1,4 @@
+# Set up an icon tray
+ 
+trayer --edge top --align right --SetDockType true --SetPartialStrut true \
+ --expand true --width 10 --transparent true --alpha 0 --tint 0x000000 --height 17 &
